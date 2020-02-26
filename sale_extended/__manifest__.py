e0da573f1407a5335351ea38c5de4e1aecab8ba3
Pablo Arcos <parcos@avancys.com.co>
2020-02-26 11:44:39 -0500

    [ADD] Modulo facturacion electronica

--- sale_extended/__manifest__.py
+++ sale_extended/__manifest__.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Base Vat Co",
+    'name': "Ventas Santillana",
 
-    'summary': "VAT validation for Partner's VAT numbers.",
+    'summary': 'Sales internal machinery',
 
-    'description': "",
+    'description': """
+        This module contains all the common features of Sales Management and eCommerce.
+    """,
 
-    'author': "Avancys",
-    'contributors': "Juan Arcos https://github.com/juanparmer",
-    'website': "http://www.avancys.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Accounting',
+    'category': 'Sales',
     'version': '12.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base_vat'],
+    'depends': ['sale', 'account', 'stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'views/templates.xml',
+        'views/views.xml',
+        'views/templates.xml',
+        'demo/demo.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
